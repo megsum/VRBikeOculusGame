8341d3c54a6d6f12128e329217612e77d3488e9d
LeahGabrielle <olexson@ualberta.ca>
2016-03-14 18:32:11 -0600

    added collision counter script and attached to gamebike object

---
+++ UnityProject/Assets/VRBike/Utils/CollisionCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCounter : MonoBehaviour {
+	public static int numCollisions = -1;
+
+	// Use this for initialization
+	void Start () {
+		if (numCollisions == -1) {
+			numCollisions = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//Increases collision counter upon entering a trigger
+	void OnTriggerEnter(Collider other){
+		numCollisions++;
+		Debug.Log (numCollisions);
+	}
+}
