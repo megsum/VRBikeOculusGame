c956220e65892cd34c034c120530843ce83963bb
Adam Ford <adamjford@gmail.com>
2016-03-14 18:20:32 -0600

    #23: Start button now loads trail

--- UnityProject/Assets/StartMenu/LoadOnClick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Assets.StartMenu
-{
-    public class LoadOnClick : MonoBehaviour
-    {
-         public void LoadScene()
-    }
-}
---
+++ UnityProject/Scripts/StartMenu/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.StartMenu
+{
+    public class LoadOnClick : MonoBehaviour
+    {
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+}
