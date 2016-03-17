b4d81f82b82467b126adac7005321562c003fed1
Adam Ford <adamjford@gmail.com>
2016-03-16 20:25:41 -0600

    #16: Halfway screen now displays time

--- UnityProject/Assets/Scripts/HalfwayScreen/HalfwayScreenDisplay.cs
+++ UnityProject/Assets/Scripts/HalfwayScreen/HalfwayScreenDisplay.cs
@@ -16,9 +16,9 @@ public class HalfwayScreenDisplay : MonoBehaviour
     void Start () {
         var trial = GameObject.Find("TrialManager").GetComponent<Trial>();
 
-        var timeElapsed = trial.FirstAttempt.TimeElapsed;
+        var timeElapsed = TimeSpan.FromSeconds(trial.FirstAttempt.TimeElapsed);
 
-        Time.GetComponent<Text>().text = TimeSpan.FromSeconds(timeElapsed).ToString();
+        Time.GetComponent<Text>().text = string.Format(@"{0}.{1:000} seconds", (int)Math.Floor(timeElapsed.TotalSeconds), timeElapsed.Milliseconds);
         ObstaclesHit.GetComponent<Text>().text = trial.FirstAttempt.ObstaclesHit.ToString();
         NextAttempt.GetComponent<Text>().text = trial.SecondAttempt.Type.ToString();
     }
