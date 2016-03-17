836e91f81a11ac4a74ea62d3ad69736c52d6cb1e
Adam Ford <adamjford@gmail.com>
2016-03-16 22:07:38 -0600

    #16: A little cleanup

--- UnityProject/Assets/Scripts/Trail/EndGoalTrigger.cs
+++ UnityProject/Assets/Scripts/Trail/EndGoalTrigger.cs
@@ -1,33 +1,35 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class EndGoalTrigger : MonoBehaviour {
-    private Trial _trial;
+public class EndGoalTrigger : MonoBehaviour
+{
     private BikeController _bikeController;
+    private Trial _trial;
 
-    void Awake()
+    private void Awake()
     {
         _bikeController = FindObjectOfType<BikeController>();
         _trial = GameObject.Find("TrialManager").GetComponent<Trial>();
     }
 
-    void OnTriggerEnter()
+    private void OnTriggerEnter()
     {
         var trialState = _trial.TrialState;
         var attempt = _trial.GetLatestAttempt();
 
+        // Source: StandardUILoader.Update()
         var attemptTime = DateTime.Now - _bikeController.ReferenceTime;
         attempt.TimeElapsed = attemptTime;
 
         if (trialState == TrialState.OnFirstAttempt)
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2); // Halfway screen
         }
+
         if (trialState == TrialState.OnSecondAttempt)
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(3); // End screen
         }
-	}
+    }
 }
