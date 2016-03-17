df1f133439e4fb13e794cdd570dea1e69faf44ba
Adam Ford <adamjford@gmail.com>
2016-03-16 19:31:29 -0600

    #16: An arbitrarily-placed end goal cube now loads a new scene

---
+++ UnityProject/Assets/Scripts/Trail/EndGoalTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGoalTrigger : MonoBehaviour {
+	void OnTriggerEnter() {
+	    SceneManager.LoadScene(2);
+	}
+}
