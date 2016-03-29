26b3b435da261303896e623f0f46d08b0bcbf19a
Adam Ford <adamjford@gmail.com>
2016-03-28 21:08:01 -0600

    #39: Ensure GameBike doesn't respawn upon second load

---
+++ UnityProject/Assets/Scripts/PreventDuplicateGameBike.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// Source: http://forum.unity3d.com/threads/dontdestroyonload-leading-to-double-objects.58609/ (2016-03-28)
+public class PreventDuplicateGameBike : MonoBehaviour
+{
+    private static PreventDuplicateGameBike _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
