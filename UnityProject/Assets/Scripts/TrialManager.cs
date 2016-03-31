c8a9b108705c582cfaa032b95da486ccddbdb263
LeahGabrielle <olexson@ualberta.ca>
2016-03-31 08:12:31 -0600

    moved end, printed out time

--- UnityProject/Assets/Scripts/TrialManager.cs
+++ UnityProject/Assets/Scripts/TrialManager.cs
@@ -43,6 +43,7 @@ public class TrialManager : MonoBehaviour
         {
             TimeElapsed = timeElapsed;
             TrialState = TrialState.Done;
+			Debug.Log("Time: " + TimeElapsed);
             Debug.Log("Collisions: " + ObstaclesHit);
         }
     }
